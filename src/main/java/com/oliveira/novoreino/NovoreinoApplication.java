be7d1c4950f340977c805b931c11cb117978f2b6
Francisco Oliveira <fransoliveira@gmail.com>
2025-03-13 15:20:02 -0300

    Reino garotada

---
+++ src/main/java/com/oliveira/novoreino/NovoreinoApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.novoreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveira/novoreino/NovoreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.novoreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
